ddcef95cdd5a8a4646d23f0c9294fedd52c9dfae
hg-study-pc1 <2216875137@qq.com>
2026-02-25 11:15:41 +0800

    准备部署

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'ZW',
+  tagline: 'ZWの',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -20,15 +20,17 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://chenqi1551.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'chenqi1551', // Usually your GitHub org/user name.
+  projectName: 'hg-docusaurus-public-deoloy-github',
+  deploymentBranch: 'gh-pages', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
